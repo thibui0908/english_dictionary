d4f422537cf45185ab6a6bac2b93c33c288fd1d0
thibui0908 <thi.t.bui@sjsu.edu>
2021-06-30 00:49:49 -0700

    Final functional program

--- app.py
+++ app.py
@@ -8,10 +8,22 @@ def translate(w):
     if w in data: 
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        answer = input("Did you mean %s instead? Y for yes, N for No. " % get_close_matches(w, data.keys())[0])
+        if answer == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif answer == "N":
+            return "The word doesn't exist. Please double check"
+        else: 
+            return "We didn't understand your query."
     else:
         return "The word doesn't exist. Please double check"
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output: 
+        print(item)
+else:
+    print(output)
