a7dcdf186052c3ce942b2a36a7afbaba3c7492df
thibui0908 <thi.t.bui@sjsu.edu>
2021-06-30 00:42:04 -0700

    Add close matches

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translate(word): 
-    return data(word)
+def translate(w): 
+    w = w.lower()
+    if w in data: 
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+    else:
+        return "The word doesn't exist. Please double check"
 
 word = input("Enter word: ")
 
